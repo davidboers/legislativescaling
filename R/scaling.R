3168145b4ff07fcf20e2b03c10a6e568199e6284
davidboers <kwmmineccraft@gmail.com>
2025-02-22 13:16:08 -0500

    Custom scaling

--- R/scaling.R
+++ R/scaling.R
@@ -1,5 +1,10 @@
 library(cluster)
+library(usedist)
 
-makeplot <- function(obj) {
-  return(cmdscale(dist(obj)))
+makeplot <- function(obj, distance_func) {
+  return(cmdscale(custom_dist(obj, distance_func)))
+}
+
+custom_dist <- function(obj, distance_func) {
+  return(dist_make(obj, distance_func))
 }
