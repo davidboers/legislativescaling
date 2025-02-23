f79feea0d1882b4837c9f18102628cab02dfa400
davidboers <kwmmineccraft@gmail.com>
2025-02-23 11:15:10 -0500

    Members database

--- bin/main.R
+++ bin/main.R
@@ -1,7 +1,13 @@
 library(devtools)
+library(jsonlite)
 load_all("../R")
-db_files <- c("2023_24_house.rds", "2023_24_senate.rds", "2023_spc_house.rds",
-              "2023_spc_senate.rds", "2025_26_house.rds", "2025_26_senate.rds")
+#db_files <- c("2023_24_house.rds", "2023_24_senate.rds", "2023_spc_house.rds",
+#              "2023_spc_senate.rds", "2025_26_house.rds", "2025_26_senate.rds")
+db_files <- c("2023_spc_senate.rds")
+
+members <- jsonlite::fromJSON(txt = "../data/member-info.json")
+rownames(members) <- members$id
+members <- subset(members, select = -c(id))
 
 distance_func_ga <- function(r1, r2) {
   r <- t(rbind(r1, r2))
