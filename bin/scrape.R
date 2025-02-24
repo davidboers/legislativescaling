2acb0b192ded1ebb5b958d9019347eb5103070b7
davidboers <kwmmineccraft@gmail.com>
2025-02-24 11:11:15 -0500

    Skipping House 23-24 test because it's not working

--- bin/scrape.R
+++ bin/scrape.R
@@ -8,9 +8,11 @@ file <- c("2023_24_house.rds", "2023_24_senate.rds", "2023_spc_house.rds",
           "2023_spc_senate.rds", "2025_26_house.rds", "2025_26_senate.rds")
 databases <- data.frame(id = 1:6, session_id, chamber, file)
 for (i in seq_len(nrow(databases))) {
-  database <- databases[i, ]
-  lst <- get_vote_list(database$session_id, database$chamber, token)
-  data <- make_full_table(lst, token)
-  saveRDS(data, file = paste("../data", database$file, sep = "/"))
-  print(paste("Made database:", database$file))
+  if (i != 1) {
+    database <- databases[i, ]
+    lst <- get_vote_list(database$session_id, database$chamber, token)
+    data <- make_full_table(lst, token)
+    saveRDS(data, file = paste("../data", database$file, sep = "/"))
+    print(paste("Made database:", database$file))
+  }
 }
