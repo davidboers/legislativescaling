1c49bd758b44f0d3830173d21b7e8969137038d2
davidboers <kwmmineccraft@gmail.com>
2025-02-24 11:11:47 -0500

    Added 2023 Senate Special session test

--- tests/testthat/test-fromGALegis.R
+++ tests/testthat/test-fromGALegis.R
@@ -9,3 +9,10 @@ test_that("Get Senate vote list for 2023-24 term", {
   lst <- get_vote_list(id_2023_24, "2", token)
   expect_that(nrow(lst), equals(902))
 })
+
+test_that("Get Senate votes for 2023 special session", {
+  lst <- get_vote_list(id_2023_spc, "2", token)
+  data <- make_full_table(lst, token)
+  expect_that(nrow(data), equals(56))
+  saveRDS(data, file = "../../data/2023_spc_senate.rds")
+})
