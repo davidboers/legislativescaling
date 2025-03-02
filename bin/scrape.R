98aa5da5aed90571387b26ece4837fa616348395
davidboers <kwmmineccraft@gmail.com>
2025-03-02 18:26:29 -0500

    Restored scraping for 2023-24 session House

--- bin/scrape.R
+++ bin/scrape.R
@@ -8,14 +8,12 @@ file <- c("2023_24_house.rds", "2023_24_senate.rds", "2023_spc_house.rds",
           "2023_spc_senate.rds", "2025_26_house.rds", "2025_26_senate.rds")
 databases <- data.frame(id = 1:6, session_id, chamber, file)
 for (i in seq_len(nrow(databases))) {
-  if (i != 1) {
-    database <- databases[i, ]
-    members <- get_member_list(database$session_id, database$chamber, token)
-    lst <- get_vote_list(database$session_id, database$chamber, token)
-    data <- make_full_table(lst, token)
-    saveRDS(data, file = paste("../data", database$file, sep = "/"))
-    membersfile <- paste("members_", database$file, sep = "")
-    saveRDS(members, file = paste("../data", membersfile, sep = "/"))
-    print(paste("Made database:", database$file))
-  }
+  database <- databases[i, ]
+  members <- get_member_list(database$session_id, database$chamber, token)
+  lst <- get_vote_list(database$session_id, database$chamber, token)
+  data <- make_full_table(lst, token)
+  saveRDS(data, file = paste("../data", database$file, sep = "/"))
+  membersfile <- paste("members_", database$file, sep = "")
+  saveRDS(members, file = paste("../data", membersfile, sep = "/"))
+  print(paste("Made database:", database$file))
 }
