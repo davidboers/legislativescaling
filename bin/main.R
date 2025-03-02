443469d87ea8f8120405f859a985ab033dfa07ff
davidboers <kwmmineccraft@gmail.com>
2025-03-02 18:26:04 -0500

    Restored scraping for 2023-24 session House

--- bin/main.R
+++ bin/main.R
@@ -5,10 +5,10 @@ db_files <- c("2023_24_house.rds", "2023_24_senate.rds", "2023_spc_house.rds",
               "2023_spc_senate.rds", "2025_26_house.rds", "2025_26_senate.rds")
 
 for (file in db_files) {
-  file <- paste("../data", file, sep = "/")
+  path <- paste("../data", file, sep = "/")
   membersfile <- paste("members_", file, sep = "")
   members <- readRDS(paste("../data", membersfile, sep = "/"))
-  write_profiles_ga(file, members)
+  write_profiles_ga(path, members)
 }
 
 warnings()
