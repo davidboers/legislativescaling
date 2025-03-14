935fb10a787d8ef8cca270c056c455e30bd5a91e
davidboers <kwmmineccraft@gmail.com>
2025-03-14 13:44:53 -0400

    It is now long enough into 2025 session that we can switch these

--- tests/testthat/test-GALegisScaling.R
+++ tests/testthat/test-GALegisScaling.R
@@ -6,9 +6,16 @@ test_that("Get Senate profiles for 2023 special session", {
   expect_that(3, equals(3))
 })
 
-test_that("Get Senate profiles for 2023-24 session", {
-  file <- "../../data/2023_24_senate.rds"
-  members <- readRDS("../../data/members_2023_24_senate.rds")
-  write_profiles_ga(file, members)
-  expect_that(3, equals(3))
+test_that("Get Senate profiles for 2025-26 session", {
+  file <- "../../data/2025_26_senate.rds"
+  members <- readRDS("../../data/members_2025_26_senate.rds")
+  profiles <- write_profiles_ga(file, members)
+  expect_that(nrow(profiles), equals(56))
+})
+
+test_that("Get House profiles for 2025-26 session", {
+  file <- "../../data/2025_26_house.rds"
+  members <- readRDS("../../data/members_2025_26_house.rds")
+  profiles <- write_profiles_ga(file, members)
+  expect_that(nrow(profiles), equals(180))
 })
--- tests/testthat/test-fromGALegis.R
+++ tests/testthat/test-fromGALegis.R
@@ -1,18 +1,18 @@
 token <- get_token()
 
-test_that("Get House vote list for 2023-24 term", {
-  lst <- get_vote_list(id_2023_24, "1", token)
-  expect_that(nrow(lst), equals(890))
-})
-
-test_that("Get Senate vote list for 2023-24 term", {
-  lst <- get_vote_list(id_2023_24, "2", token)
-  expect_that(nrow(lst), equals(902))
-})
-
 test_that("Get Senate votes for 2023 special session", {
   lst <- get_vote_list(id_2023_spc, "2", token)
   data <- make_full_table(lst, token)
   expect_that(nrow(data), equals(56))
   saveRDS(data, file = "../../data/2023_spc_senate.rds")
+})
+
+test_that("Get House vote list for 2025-26 term", {
+  lst <- get_vote_list(id_2025_26, "1", token)
+  expect_that(nrow(lst), equals(890))
+})
+
+test_that("Get Senate vote list for 2025-26 term", {
+  lst <- get_vote_list(id_2025_26, "2", token)
+  expect_that(nrow(lst), equals(902))
 })
