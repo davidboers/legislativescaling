3f40e497b17688f89fcda6675e463c4d3e12f679
davidboers <kwmmineccraft@gmail.com>
2025-05-01 14:54:55 -0400

    Cleanup and SMACOF use

--- R/scaling.R
+++ R/scaling.R
@@ -1,22 +1,18 @@
 library(cluster)
 library(usedist)
+library(smacof)
 
 distance_func <- function(r1, r2) {
   r <- t(rbind(r1, r2))
-  r1 <- r[, 1]
-  r2 <- r[, 2]
-  delta <- sum(!is.na(r1) & !is.na(r2) &
-                 as.character(r1) != as.character(r2))
-  if (is.na(delta) || delta == 0) {
-    return(1)
-  } else {
-    return(delta)
-  }
+  delta <- sum(!is.na(r[, 1]) & !is.na(r[, 2]) &
+                 as.character(r[, 1]) != as.character(r[, 2]))
+  return(ifelse(is.na(delta) || delta == 0, 1, delta))
 }
 
 makeplot <- function(obj) {
-  my_distance_func <- function(r1, r2) distance_func(r1, r2)
-  return(cmdscale(custom_dist(obj, my_distance_func)))
+  dist_matrix <- custom_dist(obj, distance_func)
+  smacof_result <- smacofSym(dist_matrix)
+  return(smacof_result$conf)
 }
 
 custom_dist <- function(obj, distance_func) {
